2cbfc02ed84100ca46768434029b823c7c24c30c
James M Snell <jasnell@gmail.com>
2015-07-22 21:04:37 -0700

    do you even npm test?

---
+++ test/test.js
@@ -0,0 +1,17 @@
+var child = require('child_process');
+var assert = require('assert');
+
+console.log(process.cwd());
+describe('It should not die', function() {
+  it('should just work', function(done) {
+    this.timeout(60 * 1000); // increase the timeout
+    var proc = child.spawn(
+      'bin/citgm',
+      ['lodash', 'known/lodash/test.js'],
+      {stdio:[0,1,2]});
+    proc.on('close', function(code) {
+      assert(!code);
+      done();
+    });
+  });
+});
