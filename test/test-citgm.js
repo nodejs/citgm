52ee702f8b864ba924fd3d5330d2ef7bda18f2e6
Myles Borins <mborins@us.ibm.com>
2015-10-30 17:13:31 -0700

    test: first pass at integration test
    * does omg-i-pass pass
    * does omg-i-fail fail

--- test/test-citgm.js
+++ test/test-citgm.js
@@ -1,6 +1,29 @@
+var path = require('path');
+
 var test = require('tap').test;
 
-test('omg such a stub', function (t) {
+var spawn = require('../lib/spawn');
+
+var citgmPath = path.resolve(__dirname, '..', 'bin', 'citgm');
+
+test('citgm: omg-i-pass', function (t) {
   t.plan(1);
-  t.ok(true, 'I know such stubbbbb');
+  var proc = spawn(citgmPath, ['omg-i-pass']);
+  proc.on('error', function(err) {
+    t.fail('we should not get an error testing omg-i-pass');
+  });
+  proc.on('close', function (code) {
+    t.ok(code === 0, 'omg-i-pass should pass and exit with a code of zero');
+  });
+});
+
+test('citgm: omg-i-fail', function (t) {
+  t.plan(1);
+  var proc = spawn(citgmPath, ['omg-i-fail']);
+  proc.on('error', function(err) {
+    t.fail('we should not get an error testing omg-i-pass');
+  });
+  proc.on('close', function (code) {
+    t.equal(code, 1, 'omg-i-fail should fail and exit with a code of one');
+  });
 });
