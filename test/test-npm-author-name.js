5f3cddeacdb6597f3fff5ba74071d5cdad8f6ff2
Myles Borins <mborins@us.ibm.com>
2016-02-29 14:56:13 -0800

    test: add test for npm-author-naame

---
+++ test/test-npm-author-name.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var test = require('tap').test;
+var rewire = require('rewire');
+
+var npmTest = rewire('../lib/npm/test');
+
+var authorName = npmTest.__get__('authorName');
+
+test('npm.test() authorName:', function (t) {
+  var name = 'titus';
+  var author = {
+    name: 'Randy Savage',
+    email: 'randy@wwe.rekt',
+    url: 'omg.html'
+  };
+  var authorExpected = 'Randy Savage <randy@wwe.rekt> (omg.html)';
+  t.equals(authorName(name), name, 'it should return any string');
+  t.equals(authorName(author), authorExpected, 'it should return the expected string when given an object');
+  t.end();
+});
+
+test('npm.test() authorName partial data:', function (t) {
+  var name = 'titus';
+  var authorOne = {
+    name: 'Randy Savage'
+  };
+  var authorOneExpected = 'Randy Savage';
+  var authorTwo = {
+    email: 'thedude@abides.net'
+  };
+  var authorTwoExpected = '<thedude@abides.net>';
+  t.equals(authorName(name), name, 'it should return any string');
+  t.equals(authorName(authorOne), authorOneExpected, 'it should return the expected string when given an object');
+  t.equals(authorName(authorTwo), authorTwoExpected, 'it should return the expected string when given an object');
+  t.end();
+});
---
+++ test/test-npm-test.js
@@ -0,0 +1,7 @@
+var test = require('tap').test;
+
+var npmTest = require('../lib/npm/test');
+
+test('npm.test():', function (t) {
+  t.end();
+});
