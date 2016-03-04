55c5c5d8e053b0db7b8bc2a4d181f7d4ca82e20b
Myles Borins <mborins@us.ibm.com>
2016-03-04 13:39:14 -0800

    test: 100% coverage on lib/spawn

--- test/test-spawn.js
+++ test/test-spawn.js
@@ -1,15 +1,16 @@
 'use strict';
 
 var test = require('tap').test;
+var rewire = require('rewire');
 
-var spawn = require('../lib/spawn');
+var spawn = rewire('../lib/spawn');
 
 test('spawn:', function (t) {
   var child = spawn('echo', ['Hello world.']);
-  
+
   var error = '';
   var message = '';
-  
+
   child.stdout.on('data', function (chunk) {
     message += chunk;
   });
@@ -21,4 +22,40 @@ test('spawn:', function (t) {
   });
 
   child.on('error', t.error);
+});
+
+test('spawn: windows mock', function (t) {
+  var child = spawn.__get__('child');
+  var sp = child.spawn;
+  var platform = process.platform;
+  Object.defineProperty(process, 'platform', {
+    value: 'win32'
+  });
+
+  child.spawn = function (cmd, args, options) {
+    return  {
+      cmd: cmd,
+      args: args,
+      options: options
+    };
+  };
+  
+  var result = spawn('echo', ['Hello world.']);
+  var expected = {
+    cmd: 'cmd',
+    args: [
+      '/c',
+      'echo',
+      'Hello world.'
+    ],
+    options: undefined
+  };
+
+  child.spawn = sp;
+  Object.defineProperty(process, 'platform', {
+    value: platform
+  });
+  
+  t.deepEqual(result, expected, 'we should have the expected options for win32');
+  t.end();
 });
