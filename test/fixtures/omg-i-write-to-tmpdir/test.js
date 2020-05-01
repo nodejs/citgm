b187a549c7ada4c0a224fcca4556554dacd3ed5a
Michaël Zasso <targos@protonmail.com>
2020-05-01 16:18:59 +0200

    ci: update GitHub actions workflow
    - Use Node.js 14 in actions/setup-node
    
    - Add Node.js 14.x to the test matrix
    
    - Remove CI: true from the environment
    
    The variable is already set by GitHub
    
    - Fix bug in test fixture
    
    Discovered with v14.x because of
    https://github.com/nodejs/node/pull/31030.

--- test/fixtures/omg-i-write-to-tmpdir/test.js
+++ test/fixtures/omg-i-write-to-tmpdir/test.js
@@ -4,4 +4,4 @@ const { join } = require('path');
 const { tmpdir } = require('os');
 const { writeFileSync } = require('fs');
 
-writeFileSync(join(tmpdir(), 'omg-i-write-to-tmpdir-testfile'));
+writeFileSync(join(tmpdir(), 'omg-i-write-to-tmpdir-testfile'), '');
